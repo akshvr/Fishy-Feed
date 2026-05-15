d11335ef8eb7fbeb15cdb4ee23f2b1e7cd01b530
akshvr <akshara.vramesh@gmail.com>
2026-05-15 16:10:03 -0700

    make microbit show till A is pressed
    if we miss seeing display during feeding, we should still be able to check later

--- microbit_makecode.js
+++ microbit_makecode.js
@@ -1,29 +1,31 @@
-serial.redirectToUSB()
-
-basic.showIcon(IconNames.Asleep)
-
+input.onButtonPressed(Button.A, function () {
+    lastStatus = "RESET"
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showIcon(IconNames.Asleep)
+})
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     let msg = serial.readLine().trim()
-
-    if (msg == "OK") {
+lastStatus = msg
+    if (msg == "WATCHING") {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showIcon(IconNames.Target)
+    } else if (msg == "OK") {
         music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(659, music.beat(BeatFraction.Half))
         basic.showIcon(IconNames.Happy)
-
     } else if (msg == "NO_FOOD") {
         music.playTone(196, music.beat(BeatFraction.Whole))
-        basic.showString("NO FOOD")
-        basic.showIcon(IconNames.Sad)
-
+        basic.showIcon(IconNames.No)
     } else if (msg == "NOT_EATING") {
         music.playTone(175, music.beat(BeatFraction.Whole))
-        basic.showString("EAT?")
         basic.showIcon(IconNames.Sad)
-
-    } else if (msg == "WATCHING") {
-        basic.showString("WATCH")
-
     } else {
         music.playTone(131, music.beat(BeatFraction.Whole))
-        basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.Confused)
     }
 })
+let lastStatus = ""
+serial.redirectToUSB()
+lastStatus = "RESET"
+basic.showIcon(IconNames.Asleep)
